ad8a5e2f002df3cbb8596b97935fe556e8088b7c
Lin-Enting <30748101@gm.nfu.edu.tw>
2021-08-05 19:15:33 +0800

    抓取檔名

--- src/js/main.js
+++ src/js/main.js
@@ -30,6 +30,19 @@ $(function () {
   }
 })
 
+//-- Btn Tag
+// 抓取檔名
+$(function () {
+  let url = location.href
+  $('a').each(function () {
+    let aHref = $(this).attr('href').split('/')
+    let aValue = aHref[aHref.length - 1] || ' '
+    if (url.indexOf(aValue) > -1 && aValue !== '##') {
+      $(this).addClass('is-active')
+    }
+  })
+})
+
 
   // -- burger
 $(function() {
